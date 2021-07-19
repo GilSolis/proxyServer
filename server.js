3eea284c9e470567ed53ff21e93be043212d7a42
Gil Solis <gilsolisuw@gmail.com>
2021-07-18 20:29:09 -0700

    heroku

---
+++ server.js
@@ -0,0 +1,19 @@
+const cors = require('cors');
+const axios = require('axios');
+const express = require('express');
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.post('/fetch', async (req, res)=>{
+  const {searchURL, method} = req.body;
+  try{
+    const data = await axios[method](searchURL);
+    res.json(data);
+  }catch(err){
+    res.json(err);
+  }
+})
+
+app.listen(process.env.PORT || 5000)
